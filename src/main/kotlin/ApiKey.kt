f95f479bea913c66b255de7ae8657c246164a43c
lintao.song <lintao.song@huifu.com>
2023-03-03 18:12:47 +0800

    fix优化UI

--- src/main/kotlin/ApiKey.kt
+++ src/main/kotlin/ApiKey.kt
@@ -1,1 +1,1 @@
-const val API_KEY = "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"
+const val API_KEY = "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"
--- src/main/kotlin/ChatList.kt
+++ src/main/kotlin/ChatList.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import io.kamel.image.KamelImage
 import io.kamel.image.lazyPainterResource
@@ -21,76 +20,78 @@ fun ColumnScope.ChatList(
 ) {
     Box(modifier = Modifier.weight(1.0f)) {
         LazyColumn(
-            modifier = Modifier.fillMaxSize().padding(8.dp),
+            modifier = Modifier.fillMaxSize().padding(16.dp),
             state = listState,
             verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
             items(chatList.size) {
                 val message = chatList[it]
                 val horizontal = if (message.from != "我") Arrangement.Start else Arrangement.End
-                Row(horizontalArrangement = horizontal, modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    horizontalArrangement = horizontal,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     if (message.from == "我") {
-                        Box(
-                            modifier = Modifier.size(50.dp)
-                                .background(Color.Green, shape = CircleShape),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(message.from)
+                        Row(modifier = Modifier.padding(start = 80.dp)) {
+                            Text(message.message, modifier = Modifier.padding(top = 10.dp))
+                            Spacer(modifier = Modifier.size(16.dp))
+                            Avatar(message)
                         }
-                        Spacer(modifier = Modifier.size(16.dp))
-                        Text(message.message, modifier = Modifier.weight(1f).padding(top = 10.dp, end = 50.dp))
                     } else {
-                        if (!message.message.startsWith("http")) {
-                            Text(
-                                message.message,
-                                modifier = Modifier.weight(1f).padding(top = 10.dp, start = 50.dp),
-                                textAlign = TextAlign.End
-                            )
-                        } else {
-                            Row(
-                                modifier = Modifier.weight(1f).padding(top = 10.dp, start = 50.dp),
-                                horizontalArrangement = Arrangement.End
-                            ) {
-                                KamelImage(
-                                    resource = lazyPainterResource(data = message.message),
-                                    modifier = Modifier.size(150.dp, 150.dp),
-                                    contentDescription = "Image",
-                                    onLoading = {
-                                        Box(
-                                            modifier = Modifier.background(Color.LightGray).size(150.dp, 150.dp),
-                                            contentAlignment = Alignment.Center
-                                        ) {
-                                            CircularProgressIndicator(
-                                                modifier = Modifier.size(30.dp),
-                                                color = Color.White
-                                            )
-                                        }
-                                    },
-                                    onFailure = {
-                                        Box(
-                                            modifier = Modifier.background(Color.LightGray).size(150.dp, 150.dp),
-                                            contentAlignment = Alignment.Center
-                                        ) {
-                                            Text(
-                                                "加载失败",
-                                                color = Color.Red
-                                            )
-                                        }
-                                    }
-                                )
+                        Row(modifier = Modifier.padding(end = 80.dp)) {
+                            Avatar(message)
+                            Spacer(modifier = Modifier.size(16.dp))
+                            if (!message.message.startsWith("http")) {
+                                Text(message.message, modifier = Modifier.padding(top = 10.dp))
+                            } else {
+                                ChatImage(message)
                             }
-                        }
-                        Spacer(modifier = Modifier.size(16.dp))
-                        Box(
-                            modifier = Modifier.size(50.dp)
-                                .background(Color.Green, shape = CircleShape),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(message.from)
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ChatImage(message: Message) {
+    KamelImage(
+        resource = lazyPainterResource(data = message.message),
+        modifier = Modifier.size(150.dp, 150.dp),
+        contentDescription = "Image",
+        onLoading = {
+            Box(
+                modifier = Modifier.background(Color.LightGray).size(150.dp, 150.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(30.dp),
+                    color = Color.White
+                )
+            }
+        },
+        onFailure = {
+            Box(
+                modifier = Modifier.background(Color.LightGray).size(150.dp, 150.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    "加载失败",
+                    color = Color.Red
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun Avatar(message: Message) {
+    Box(
+        modifier = Modifier.size(50.dp)
+            .background(Color.Green, shape = CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(message.from)
     }
 }
